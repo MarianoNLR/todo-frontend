7d8814b8041adec75f0c2622973f1698b74fcaea
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-29 20:26:53 -0300

    styles and link to task view

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,10 +1,32 @@
 import PropTypes from 'prop-types'
 import './TaskCard.css'
+import { differenceInDays } from 'date-fns'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export function TaskCard (props) {
+    const navigate = useNavigate()
+    const getDifferenceDays = () => {
+        const difference = differenceInDays(new Date(props.deadline), new Date())
+        
+        if (difference >= 7) {
+            return "green"
+        }
+        else if (difference >= 3) {
+            return "yellow"
+        }
+        else {
+            return "red"
+        }
+    }
+
+    const handleClickTask = (e) => {
+        e.preventDefault()
+        navigate(`/task/${props.id}`)
+    }
     return (
         <>
-            <div className="task-card-wrapper">
+            <div className={`task-card-wrapper ${getDifferenceDays()}`} onClick={(e) => handleClickTask(e)}>
                 <div className="card-title-wrapper">{props.title}</div>
                 <div className="card-description-wrapper">
                     {props.description}
@@ -21,6 +43,7 @@ export function TaskCard (props) {
 }
 
 TaskCard.propTypes = {
+    id: PropTypes.string,
     title: PropTypes.string,
     description: PropTypes.string,
     deadline: PropTypes.string,
