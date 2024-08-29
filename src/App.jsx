cdd27fec97024cc2178bd2f2b5c18a8dcac2d920
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-29 20:26:14 -0300

    styles and using layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home/Home.jsx'
 import { AuthProvider } from './components/AuthProvider.jsx'
 import { Authentication } from './pages/Authentication/Authentication.jsx'
-
+import Layout  from './components/Layout/Layout.jsx'
+import { TaskView } from './pages/TaskView/TaskView.jsx'
 function App() {
 
   return (
     <AuthProvider>
+      <Layout>
       <Routes>
         <Route index path='/' element={<Home />}></Route>
         <Route path='/authentication' element={<Authentication />}></Route>
+        <Route path='/task/:taskId' element={<TaskView />}></Route>
       </Routes>
+      </Layout>
     </AuthProvider>
   )
 }
