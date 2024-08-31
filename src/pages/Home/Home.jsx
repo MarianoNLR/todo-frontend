f3dd0cded96d0ec9af6a496d6a4b4e938cccafc9
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-31 19:24:21 -0300

    using task list component and order by added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,27 +4,17 @@ import api from '../../../api.js'
 import { TaskCard } from "../../components/TaskCard/TaskCard.jsx"
 import './Home.css'
 import { NewTaskForm } from "../../components/NewTaskForm/NewTaskForm.jsx"
+import { TaskList } from "../../components/TaskList/TaskList.jsx"
 
 export function Home () {
-    const [taskList, setTaskList] = useState([])
     const [taskFormIsVisible, setTaskFormIsVisible] = useState(false)
-    const [loadingTaskList, setLoadingTaskList] = useState(true)
     const [orderBy, setOrderBy] = useState('')
+    const [loadingTaskList, setLoadingTaskList] = useState(true)
     const user = JSON.parse(window.localStorage.getItem('user'))
     useEffect(() => {
-        if (loadingTaskList) {
-            api.get('task/', {
-                headers: {
-                    'Authorization': `Bearer ${user.token}`
-                }
-            })
-            .then(res => {
-                setTaskList(res.data.taskList)
-                setLoadingTaskList(false)
-            }) 
-        }
         
-    }, [loadingTaskList, user.token])
+        
+    }, [user.token])
 
     const toggleTaskForm = () => {
         setTaskFormIsVisible(!taskFormIsVisible);
@@ -34,8 +24,9 @@ export function Home () {
         console.log("Nueva Tarea")
     }
 
-    const handleOrderChange = (event) => {
-        setOrderBy(event.target.value);
+    const handleOrderChange = (e) => {
+        setOrderBy(e.target.value);
+        setLoadingTaskList(true)
       };
 
     return (
@@ -43,32 +34,17 @@ export function Home () {
             <main className="main-home">
                 <div className="controls-wrapper">
                     <div className="order-by-wrapper">
-                        <select name="order-by" id="order-by" className="order-by-select">
+                        <select name="order-by" id="order-by" className="order-by-select" onChange={(e) => handleOrderChange(e)}>
                             <option value="">Ordenar</option>
-                            <option value="">Prioridad</option>
-                            <option value="">Fecha Limite</option>
+                            <option value="priority">Prioridad</option>
+                            <option value="deadline">Fecha Limite</option>
                         </select>
                     </div>
                     <Button className="add-task-button" variant="contained" color="success" onClick={(e) => toggleTaskForm(e)}>
                     Nueva Tarea
                     </Button>
                 </div>
-                <div className="task-list-container">
-                    {taskList.length > 0 ?
-                        taskList.map((item, index) => (
-                            <TaskCard key={index}
-                            id={item.id}
-                            title={item.title}
-                            description={item.description}
-                            deadline={item.deadline}
-                            priority={item.priority}
-                            subtasks={item.subtasks}
-                            ></TaskCard>
-                        ))
-                    :
-                    <h3>No Hay Tareas</h3>
-                    }
-                </div>
+                <TaskList setLoadingTaskList={setLoadingTaskList} loadingTaskList={loadingTaskList} orderBy={orderBy}></TaskList>
                 {taskFormIsVisible && 
                     <NewTaskForm setLoadingTaskList={setLoadingTaskList} user={user} toogleTaskForm={toggleTaskForm}>
                     </NewTaskForm>
