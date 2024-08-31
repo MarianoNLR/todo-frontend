94afccccab31afead440210e40de34d2d161ce1d
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-31 19:24:35 -0300

    + task list component

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,54 @@
+import './TaskList.css'
+import PropTypes from 'prop-types'
+import { TaskCard } from '../TaskCard/TaskCard.jsx'
+import { useEffect, useState } from 'react'
+import api
+ from '../../../api.js'
+export function TaskList (props) {
+    const user = JSON.parse(window.localStorage.getItem('user'))
+    const [taskList, setTaskList] = useState(null)
+
+    useEffect(() => {
+        api.get('task/', {
+            params: {
+                orderBy: props.orderBy,
+            },
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+        .then(res => {
+            setTaskList(res.data.taskList)
+            props.setLoadingTaskList(false)
+        }) 
+    }, [props, user.token])
+
+    if (props.loadingTaskList) {
+        return <></>
+    }
+    
+    return (
+        <div className="task-list-container">
+            {taskList.length > 0 ?
+                taskList.map((item, index) => (
+                    <TaskCard key={index}
+                    id={item.id}
+                    title={item.title}
+                    description={item.description}
+                    deadline={item.deadline}
+                    priority={item.priority}
+                    subtasks={item.subtasks}
+                    ></TaskCard>
+                ))
+            :
+            <h3>No Hay Tareas</h3>
+            }
+        </div>
+    )
+}
+
+TaskList.propTypes = {
+    loadingTaskList: PropTypes.bool,
+    setLoadingTaskList: PropTypes.func,
+    orderBy: PropTypes.string
+}
