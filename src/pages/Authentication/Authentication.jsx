e1a5534e1f712884870936db3377f92decfdb6ca
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-04 18:53:20 -0300

    styles and animations

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { LoginForm } from "../../components/LoginForm/LoginForm.jsx"
 import { SignUpForm } from "../../components/SignUpForm/SignUpForm.jsx"
 import { useNavigate } from "react-router-dom"
+import './Authentication.css'
 export function Authentication () {
     const [showLoginForm, setShowLoginForm] = useState(true)
     //const { user } = useAuth()
@@ -14,7 +15,7 @@ export function Authentication () {
 
     return (
         <>
-            <main>  
+            <main className='auth-page-main'>  
                 <div className={`form-wrapper ${showLoginForm ? 'login-active' : 'register-active'}`}>
                     {showLoginForm ? 
                     <LoginForm setShowLoginForm={setShowLoginForm}></LoginForm>
