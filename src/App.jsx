a66af7300dee082e12f0749ae9a18ae95dd9ae97
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-06 15:47:08 -0300

    route for error404

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './components/AuthProvider.jsx'
 import { Authentication } from './pages/Authentication/Authentication.jsx'
 import Layout  from './components/Layout/Layout.jsx'
 import { TaskView } from './pages/TaskView/TaskView.jsx'
+import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage.jsx'
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route index path='/' element={<Home />}></Route>
         <Route path='/authentication' element={<Authentication />}></Route>
         <Route path='/task/:taskId' element={<TaskView />}></Route>
+        <Route path='*' element={<NotFoundPage />}></Route>
       </Routes>
       </Layout>
     </AuthProvider>
